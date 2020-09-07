e435366c0379f9f44f46bae4895040534b833e50
Joss <Joss@users.noreply.github.com>
2020-09-07 13:52:27 +0300

    New page and new block commands added

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ Simple Notion.so API to add a note into selected page
 4. Select `Send IFTTT any email`
 5. Add `Webhooks` as action service
 6. Select `Make a web request`
-7. Add `<your_heroku_link>/add_url` to URL
+7. Add `<your_heroku_link>/add_block` or `<your_heroku_link>/add_page` to URL
 8. Select `Post` method
 9. Select `application/json` content type
-10. Add `{"token":"<token_v2>", "link":"<page_link>", "note":"<<<{{Body}}>>>"}` as body
+10. Add `{"token":"<token_v2>", "link":"<page_link>", "note":"<<<{{Body}}>>>", "title":"<<<{{Subject}}>>>}` as body
 
--- src/app.py
+++ src/app.py
@@ -1,27 +1,53 @@
+from datetime import date
+
 from notion.client import NotionClient
-from notion.block import TextBlock
+from notion.block import TextBlock, PageBlock 
 
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Notion API</h1>"
+    return "<h1>Notion API</h1><p>Use /add_block or /add_page</p>"
 
-@app.route('/add_note', methods=['POST'])
-def add_note():
+@app.route('/add_block', methods=['POST'])
+def add_block():
     try:
         token_v2 = request.json['token']
         notebook_link = request.json['link']
-        note_text = request.json['note']
+        note_title = request.json['title']
+        note_text = request.json['note']        
 
         client = NotionClient(token_v2)
         page = client.get_block(notebook_link)
+
+        today = date.today()
+        new_block = page.children.add_new(TextBlock, title=today.strftime("%d/%m/%y") + ": " + note_title)
+        new_block.set('format.block_color', 'red')        
         page.children.add_new(TextBlock, title=note_text)
 
         return 'The note added', 200
     except Exception:
         return 'Adding the note failed', 500
+
+@app.route('/add_page', methods=['POST'])
+def add_page():
+    try:
+        token_v2 = request.json['token']
+        notebook_link = request.json['link']
+        note_title = request.json['title']
+        note_text = request.json['note']
+
+        client = NotionClient(token_v2)
+        page = client.get_block(notebook_link)
+
+        today = date.today()
+        new_page = page.children.add_new(PageBlock, title=today.strftime("%d/%m/%y") + ": " + note_title)
+        new_page.children.add_new(TextBlock, title=note_text)
+
+        return 'The page added', 200
+    except Exception:
+        return 'Adding the page failed', 500        
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)        
